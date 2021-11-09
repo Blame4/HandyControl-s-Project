b2a02e0e913afe57a81218fe802268e5e444cd46
Zhen Newbe <1054722547@qq.com>
2021-11-09 22:55:47 +0800

    second

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using HandyControl.Controls;
+
 namespace HandyControlsProject
 {
     public partial class MainWindow
@@ -12,3 +13,5 @@ namespace HandyControlsProject
         {
             InitializeComponent();
         }
+    }
+}
